fa4c6341b1c57d93df19c1d6fc99c8a9df5f372e
Davicci <chigbundudavid@gmail.com>
2023-08-01 17:19:25 +0000

    Tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,29 +1,54 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * print_times_table - prints the n times table
- * @n: number of the times table
+ * print_times_table - prints the times table
+ * @n: integer for which the times table will be printed
+ *
+ * Description: prints the times table
+ *
+ * Return: void
  */
+
 void print_times_table(int n)
 {
-	if (n >= 0 && n <= 15)
-	{
-	int l, m, o;
+	int row, column, product;
 
-	for (l = 0; l <= n; l++)
-
+	if (n >= 0 && n < 15)
 	{
-	for (m = 0; m <= n; m++)
-	{
-	int result = l * m;
+		for (row = 0; row <= n; row++)
+		{
+			for (column = 0; column <= n; column++)
+			{
+				product = row * column;
 
-	o = l * m
-	if (m == 0)
-	printf("%d", result);
-	else
-	printf(", %4d", result);
-	}
-	printf("\n");
-	}
+				if (column == 0)
+					_putchar('0');
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else if (product > 99 && product < 1000)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
 	}
 }
